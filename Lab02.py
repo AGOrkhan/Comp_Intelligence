658bc44b631c0d9752f620d5a762b0bebf8829e6
AGOrkhan <upperfloor2003@gmail.com>
2023-11-06 03:59:31 +0000

    Fixed Novel Lab 2

--- Lab02.py
+++ Lab02.py
@@ -71,7 +71,11 @@ def ran_route(count):
     start = time.time()
     rt.score, rt.best_route = float('inf'), []
     rt.searches = 0
-
+    while time.time() - start <= count:
+        route = list(range(len(rt.matrix)))
+        random.shuffle(route)
+        opt_route(route)
+    print("Opt: ", rt.best_route, rt.score, rt.searches)
 
 
 def swap_route(route):
@@ -85,6 +89,32 @@ def swap_route(route):
             cost = route_calc(copy_route)
             l_cost, route = (cost, copy_route) if l_cost > cost else (l_cost, route)
             rt.score, rt.best_route = (cost, route) if rt.score > cost else (rt.score, rt.best_route)
+
+
+def opt_route(route):
+    rt.searches += 1
+
+    l_score = float('inf')
+    l_route = route
+
+    made_improvement = True
+    while made_improvement:
+        made_improvement = False
+        for i in range(1, len(route) - 2):
+            for j in range(i + 1, len(route)):
+                new_route = route[:i] + route[i:j][::-1] + route[j:]
+                new_cost = route_calc(new_route)
+
+                if new_cost < l_score:
+                    l_route = new_route
+                    l_score = new_cost
+                    made_improvement = True
+
+        route = l_route
+
+    # After finishing all the iterations, update the global best route and score.
+    rt.score, rt.best_route = (l_score, route) if rt.score > l_score else (rt.score, rt.best_route)
+
 
 # Put time in parameter
 ran_route(3)
